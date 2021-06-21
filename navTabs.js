27a65571fa9308874643c436858e77fd56f7f1b7
Scott Whiteman <scottwhmn@Scotts-MacBook-Pro.local>
2021-06-20 20:36:56 -0700

    rework init

---
+++ navTabs.js
@@ -0,0 +1,251 @@
+const navTabs = {
+  'work': `
+    <li class='project-slide current-slide'>
+      <p class='text-pointer'>Simple Macros</p>
+      <div class='project-container'>
+        <div class='image-container slide-image-container'>
+          <img src='./img/missing.png'/>
+        </div>
+        <div class='project-description'>
+          <p>
+            Simplified Diet App that streamlines diet tracking so the user can get back to their routine quickly.
+          </p>
+          <h5>Tech Stack</h5>
+          <ul class='tech-list'>
+            <li>
+              Dart
+            </li>
+            <li>
+              Flutter
+            </li>
+            <li>
+              Android
+            </li>
+          </ul>
+          <div class='button-wrapper'><button class=''><a href="https://github.com/scottWhiteman/spaced-repetition" target="_blank">Playstore</a></button></div>
+        </div>
+      </div>
+    </li>
+    <li class='project-slide'>
+      <p class='text-pointer'>Simple Macros Webpage</p>
+      <div class='project-container'>
+        <div class='image-container slide-image-container'>
+          <img src='./img/missing.png'/>
+        </div>
+        <div class='project-description'>
+          <p>
+            Simple Macros website housing future content for Simplify Apps
+          </p>
+          <h5>Tech Stack</h5>
+          <ul class='tech-list'>
+            <li>
+              TypeScript
+            </li>
+            <li>
+              React.js
+            </li>
+            <li>
+              CSS
+            </li>
+            <li>
+              Styled-Components
+            </li>
+            <li>
+              HTML
+            </li>
+          </ul>
+          <div class='button-wrapper'><button class=''><a href="https://github.com/scottWhiteman/spaced-repetition" target="_blank">Playstore</a></button></div>
+        </div>
+      </div>
+    </li>
+  `,
+  'current projects': `
+    <li class='project-slide current-slide'>
+      <p class='text-pointer'>Shopping Cart</p>
+      <div class='project-container'>
+        <div class='image-container slide-image-container'>
+          <img src='./img/missing.png'/>
+        </div>
+        <div class='project-description'>
+          <p>
+            Look at a list of items.  Filter by category and look at each item's description.  Utilizes React, Redux, React Hooks under TypeScript
+          </p>
+          <h5>Tech Stack</h5>
+          <ul class='tech-list'>
+            <li>
+              TypeScript
+            </li>
+            <li>
+              CSS
+            </li>
+            <li>
+              HTML
+            </li>
+            <li>
+              React.js
+            </li>
+            <li>
+              Redux
+            </li>
+            <li>
+              React Hooks
+            </li>
+          </ul>
+          <div class='button-wrapper'><button class='no-git-button'>Not yet available</button></div>
+        </div>
+      </div>
+    </li>
+  `,
+
+  'older projects': `
+    <li class='project-slide current-slide'>
+      <p class='text-pointer'>Spaced Repetition</p>
+      <div class='project-container'>
+        <div class='image-container slide-image-container'>
+          <img src='./img/spacedrep.jpg'/>
+        </div>
+        <div class='project-description'>
+          <p>Language learning app that uses flash cards with language words, utilizing spaced repetition method.</p>
+          <h5>Tech Stack</h5>
+          <ul class='tech-list'>
+            <li>
+              JavaScript
+            </li>
+            <li>
+              CSS
+            </li>
+            <li>
+              HTML
+            </li>
+            <li>
+              React.js
+            </li>
+            <li>
+              Node.js
+            </li>
+            <li>
+              Express
+            </li>
+            <li>
+              PostgreSQL
+            </li>
+          </ul>
+          <div class='button-wrapper'><button class='git-button'><a href="https://github.com/scottWhiteman/spaced-repetition" target="_blank">Checkout Git</a></button></div>
+        </div>
+      </div>
+    </li>
+    <li class='project-slide'>
+      <p class='text-pointer'>Color Game</p>
+      <div class='project-container'>
+        <div class='image-container slide-image-container'>
+          <img src='./img/colorgame.jpg'/>
+        </div>
+        <div class='project-description'>
+          <p>
+            Color game where player must choose colors matching given condition.  Gain score until incorrect answer is picked.
+          </p>
+          <h5>Tech Stack</h5>
+          <ul class='tech-list'>
+            <li>
+              JavaScript
+            </li>
+            <li>
+              CSS
+            </li>
+            <li>
+              HTML
+            </li>
+            <li>
+              React.js
+            </li>
+            <li>
+              Node.js
+            </li>
+            <li>
+              Express
+            </li>
+            <li>
+              PostgreSQL
+            </li>
+          </ul>
+          <div class='button-wrapper'><button class='git-button'><a href="https://github.com/scottWhiteman/spaced-repetition" target="_blank">Checkout Git</a></button></div>
+        </div>
+      </div>
+    </li>
+    <li class='project-slide'>
+      <p class='text-pointer'>Endless Encounters</p>
+      <div class='project-container'>
+        <div class='image-container slide-image-container'>
+          <img src='./img/endless.jpg'/>
+        </div>
+        <div class='project-description'>
+          <p>
+            Text turn based RPG game where player is pitted against an endless set of enemies.  Progress is saved to database.
+          </p>
+          <h5>Tech Stack</h5>
+          <ul class='tech-list'>
+            <li>
+              JavaScript
+            </li>
+            <li>
+              CSS
+            </li>
+            <li>
+              HTML
+            </li>
+            <li>
+              React.js
+            </li>
+            <li>
+              React Springs
+            </li>
+            <li>
+              Node.js
+            </li>
+            <li>
+              Express
+            </li>
+            <li>
+              PostgreSQL
+            </li>
+          </ul>
+          <div class='button-wrapper'><button class='git-button'><a href="https://github.com/scottWhiteman/spaced-repetition" target="_blank">Checkout Git</a></button></div>
+        </div>
+      </div>
+    </li>
+    <li class='project-slide'>
+      <p class='text-pointer'>Bookmarks</p>
+      <div class='project-container'>
+        <div class='image-container slide-image-container'>
+          <img src='./img/bookmarks.jpg'/>
+        </div>
+        <div class='project-description'>
+          <p>
+            A simple bookmark app that saves website urls under names and ratings with descriptions.
+          </p>
+          <h5>Tech Stack</h5>
+          <ul class='tech-list'>
+            <li>
+              JavaScript
+            </li>
+            <li>
+              CSS
+            </li>
+            <li>
+              HTML
+            </li>
+            <li>
+              Node.js
+            </li>
+            <li>
+              Express
+            </li>
+          </ul>
+          <div class='button-wrapper'><button class='git-button'><a href="https://github.com/scottWhiteman/spaced-repetition" target="_blank">Checkout Git</a></button></div>
+        </div>
+      </div>
+    </li>
+  `,
+};
+
+export default navTabs;
--- script.js
+++ script.js
@@ -1,5 +1,146 @@
+import navTabs from "./navTabs.js";
+
+let slideshow = document.querySelector('.project-slideshow-track');
+let slides = Array.from(slideshow.children);
+
+let dotsList = document.querySelector('.slideshow-nav');
+let dots = Array.from(dotsList.children);
+
+const nav = document.querySelector('.navigator');
+const nextButton = document.querySelector('.slideshow-right-button');
+const prevButton = document.querySelector('.slideshow-left-button');
+
 const init = () => {
-  console.log('js start')
+  slideshow.innerHTML = navTabs['older projects'];
+  setupSlides();
+  setupCircles();
+  setupNav();
+  getProjectName();
 }
+
+const setupNav = () => {
+  nav.addEventListener('click', e => {
+    const targetButton = e.target.closest('button');
+    if (!targetButton) return;
+  
+    const buttonText = targetButton.children[0].text.toLowerCase();
+  
+    if (navTabs[buttonText]) {
+      slideshow.innerHTML = navTabs[buttonText];
+      setupSlides();
+      setupCircles();
+    }
+    else {
+      slideshow.innerHTML = '';
+      setupSlides();
+      setupCircles();
+    }
+  });
+}
+
+const setupSlides = () => {
+  // slideshow = document.querySelector('.project-slideshow-track');
+  slideshow.style.transform = `translateX(0)`;
+  slides = Array.from(slideshow.children);
+  // slideWidth = slideshow.offsetWidth;
+  slides.forEach((slide, index) => {
+    slide.style.left = 100 * index + '%';
+  });
+  getProjectName();
+}
+
+const setupCircles = () => {
+  // dotsList = document.querySelector('.slideshow-nav');
+  if (slides.length === 0) {
+    dotsList.innerHTML = '';
+    return;
+  };
+  let newDotsHtml = '';
+  slides.forEach(slide => {
+    newDotsHtml += `<button class='slideshow-circle'></button>`;
+  });
+  dotsList.innerHTML = newDotsHtml;
+  Array.from(dotsList.children)[0].classList.add('selected-circle');
+  dots = Array.from(dotsList.children);
+  dotsList.addEventListener('click', e => {
+    const targetCircle = e.target.closest('button');
+    if (!targetCircle) return;
+  
+    const currentSlide = slideshow.querySelector('.current-slide');
+    const currentCircle = dotsList.querySelector('.selected-circle');
+  
+    const targetIndex = dots.findIndex(circle => circle === targetCircle);
+    const targetSlide = slides[targetIndex];
+  
+    moveToSlide(slideshow, currentSlide, targetSlide);
+    moveToCircle(currentCircle, targetCircle);
+    // getProjectName();
+  });
+}
+
+const moveToSlide = (slideshow, currentSlide, targetSlide) => {
+  if (!targetSlide) return;
+  slideshow.style.transform = `translateX(-${targetSlide.style.left})`;
+  currentSlide.classList.remove('current-slide');
+  targetSlide.classList.add('current-slide');
+  getProjectName();
+}
+
+const moveToCircle = (currentCircle, targetCircle) => {
+  if (!targetCircle) return;
+  currentCircle.classList.remove('selected-circle');
+  targetCircle.classList.add('selected-circle');
+  // getProjectName();
+}
+
+const getProjectName = () => {
+  const projectName = slideshow.querySelector('.current-slide .text-pointer').textContent;
+  const projectHeader = document.querySelector('.display-container h3');
+  projectHeader.textContent = projectName;
+}
+
+prevButton.addEventListener('click', e => {
+  const currentSlide = slideshow.querySelector('.current-slide');
+  const prevSlide = currentSlide.previousElementSibling ? currentSlide.previousElementSibling : slides[slides.length - 1];
+  moveToSlide(slideshow, currentSlide, prevSlide);
+  const currentCircle = dotsList.querySelector('.selected-circle');
+  const prevCircle = currentCircle.previousElementSibling ? currentCircle.previousElementSibling : dots[dots.length - 1];
+  moveToCircle(currentCircle, prevCircle);
+});
+nextButton.addEventListener('click', e => {
+  const currentSlide = slideshow.querySelector('.current-slide');
+  const nextSlide = currentSlide.nextElementSibling ? currentSlide.nextElementSibling : slides[0];
+  moveToSlide(slideshow, currentSlide, nextSlide);
+  const currentCircle = dotsList.querySelector('.selected-circle');
+  const nextCircle = currentCircle.nextElementSibling ? currentCircle.nextElementSibling : dots[0];
+  moveToCircle(currentCircle, nextCircle);
+});
+
+
+// dotsList.addEventListener('click', e => {
+//   const targetCircle = e.target.closest('button');
+//   if (!targetCircle) return;
+
+//   const currentSlide = slideshow.querySelector('.current-slide');
+//   const currentCircle = dotsList.querySelector('.selected-circle');
+
+//   const targetIndex = dots.findIndex(circle => circle === targetCircle);
+//   const targetSlide = slides[targetIndex];
+
+//   moveToSlide(slideshow, currentSlide, targetSlide);
+//   moveToCircle(currentCircle, targetCircle);
+// });
+
+// dots.forEach((dot, index) => {
+//   dot.addEventListener('click', e => {
+//     // const currentCircle = dotsList.querySelector('.selected-circle');
+//     const currentSlide = slideshow.querySelector('.current-slide');
+//     const targetSlide = slides[index];
+//     const currentCircle = dotsList.querySelector('.selected-circle');
+//     const targetCircle = dots[index];
+//     moveToSlide(slideshow, currentSlide, targetSlide);
+//     moveToCircle(currentCircle, targetCircle);
+//   });
+// });
 
 init();
